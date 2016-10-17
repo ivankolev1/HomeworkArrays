a51c69ca57991bbe39f7f6ecbed70d227e4e786c
ivankolev1 <ivankolev1996@gmail.com>
2016-10-17 18:51:01 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		 int[] array = new int[10];
+	        for (int i = 0; i < 10; i++)
+	        {
+	            array[i] = i * 10;
+	            System.out.println(array[i]);
+	        }
+
+	}
+
+}
